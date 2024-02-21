23eedce8b18db27aed530410e9a52865307b983e
ZhuRuoLing <jkl-963@live.com>
2024-02-21 11:19:19 +0800

    got instrument

--- src/main/java/net/fabricmc/loader/impl/instrument/AgentProviderMain.java
+++ src/main/java/net/fabricmc/loader/impl/instrument/AgentProviderMain.java
@@ -1,9 +1,13 @@
 package net.fabricmc.loader.impl.instrument;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 public class AgentProviderMain {
 	private static void accept(Instrumentation inst) {
@@ -20,5 +24,42 @@ public class AgentProviderMain {
 
 	public static void agentmain(String args,Instrumentation inst) throws Exception {
 		accept(inst);
+	}
+
+	private static String readPidFromRes(String[] args) throws IOException {
+		InputStream is = AgentProviderMain.class.getResourceAsStream("pid");
+		if (is != null){
+			byte[] bytes = new byte[1024];
+			int len = is.read(bytes);
+            return new String(bytes, 0, len);
+		}
+		return args[0];
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String pid = readPidFromRes(args);
+		if (pid.isEmpty())throw new RuntimeException("No pid provided!");
+		try {
+			Class<?> virtualMachineType = Class.forName(args[2]);
+			Object virtualMachineInstance = virtualMachineType
+					.getMethod("attach", String.class)
+					.invoke(null, pid);
+			virtualMachineType.getMethod("loadAgent", String.class, String.class)
+					.invoke(virtualMachineInstance, args[1], null);
+//			VirtualMachine vm = VirtualMachine.attach(pid);
+//			vm.loadAgent(args[2]);
+		}catch (Exception e){
+			File errorDumpFile = new File("C:/errors/errdump.txt");
+			Files.createDirectories(errorDumpFile.toPath().getParent());
+			if (errorDumpFile.exists()){
+				errorDumpFile.delete();
+			}
+			errorDumpFile.createNewFile();
+			PrintStream ps = new PrintStream(errorDumpFile);
+			e.printStackTrace(ps);
+			e.printStackTrace(System.out);
+			System.exit(2);
+		}
+		System.exit(0);
 	}
 }
--- src/main/java/net/fabricmc/loader/impl/instrument/ClassRedefineDelegateImpl.java
+++ src/main/java/net/fabricmc/loader/impl/instrument/ClassRedefineDelegateImpl.java
@@ -1,15 +1,20 @@
 package net.fabricmc.loader.impl.instrument;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.CodeSource;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -41,6 +46,7 @@ public class ClassRedefineDelegateImpl<T extends ClassLoader & KnotClassDelegate
 
 	public Instrumentation install() {
 		try {
+			clearAgentJars();
 			String name = ManagementFactory.getRuntimeMXBean().getName();
 			String pid = name.split("@")[0];
 			Log.info(LogCategory.KNOT, "Attaching agent to vm");
@@ -48,28 +54,16 @@ public class ClassRedefineDelegateImpl<T extends ClassLoader & KnotClassDelegate
 			if (!accessor.isAvailable()) {
 				throw new RuntimeException("No Attachment available.");
 			}
-			if (accessor.isExternalAttachmentRequired()){
-				throw new RuntimeException("Require external attatchment");
-			}
-			Class<?> virtualMachineType = accessor.getVirtualMachineType();
-
-			try {
-				Class<?> cls = Class.forName("sun.tools.attach.HotSpotVirtualMachine");
-				Field field = cls.getDeclaredField("ALLOW_ATTACH_SELF");
-				field.setAccessible(true);
-				Field modifiersField = Field.class.getDeclaredField("modifiers");
-				modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-				field.setBoolean(null, true);
-			} catch (Exception e) {
-				Log.warn(LogCategory.KNOT, "Could not modify ALLOW_ATTACH_SELF to true.", e);
-			}
-
-			Object virtualMachineInstance = virtualMachineType
-					.getMethod("attach", String.class)
-					.invoke(null, pid);
+			String jarFilePath = extractJar(pid);
+//			if (accessor.isExternalAttachmentRequired()){
+//				throw new RuntimeException("Require external attatchment");
+//			}
 			instrumentationFuture = new CompletableFuture<>();
-			virtualMachineType.getMethod("loadAgent", String.class, String.class)
-					.invoke(virtualMachineInstance, extractJar(), null);
+			if (accessor.isExternalAttachmentRequired()) {
+				externalAttach(accessor.getExternalAttachment(), pid, new File(jarFilePath), new File(jarFilePath));
+			} else {
+				attachSelf(accessor, pid, jarFilePath);
+			}
 			return instrumentationFuture.get(10000, TimeUnit.MILLISECONDS);
 		} catch (TimeoutException te) {
 			throw new RuntimeException("Timeout while waiting agent to attach.", te);
@@ -80,11 +74,59 @@ public class ClassRedefineDelegateImpl<T extends ClassLoader & KnotClassDelegate
 		}
 	}
 
-	public String extractJar() {
+	private static void externalAttach(AttachmentProvider.Accessor.ExternalAttachment externalAttachment,
+									   String processId,
+									   File agent,
+									   File attachmentJar) throws Exception {
+		try {
+			StringBuilder classPath = new StringBuilder().append(attachmentJar.getCanonicalPath());
+			for (File jar : externalAttachment.getClassPath()) {
+				classPath.append(File.pathSeparatorChar).append(jar.getCanonicalPath());
+			}
+			String[] command = new String[]{System.getProperty("java.home")
+					+ File.separatorChar + "bin"
+					+ File.separatorChar + (System.getProperty("os.name", "").toLowerCase(Locale.US).contains("windows") ? "java.exe" : "java"),
+					"-cp",
+					classPath.toString(),
+					"net.fabricmc.loader.impl.instrument.AgentProviderMain",
+					processId,
+					agent.getCanonicalPath(),
+					externalAttachment.getVirtualMachineType()};
+			System.out.println(Arrays.deepToString(command));
+			Process process = new ProcessBuilder(command).start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line = reader.readLine();
+			while (process.isAlive()){
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			if (process.exitValue() != 0) {
+				throw new IllegalStateException("Could not self-attach to current VM using external process");
+			}
+		} finally {
+			if (attachmentJar != null) {
+				if (!attachmentJar.delete()) {
+					attachmentJar.deleteOnExit();
+				}
+			}
+		}
+	}
+
+	private void attachSelf(AttachmentProvider.Accessor accessor, String pid, String jarFilePath) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		Class<?> virtualMachineType = accessor.getVirtualMachineType();
+		Object virtualMachineInstance = virtualMachineType
+				.getMethod("attach", String.class)
+				.invoke(null, pid);
+		virtualMachineType.getMethod("loadAgent", String.class, String.class)
+				.invoke(virtualMachineInstance, jarFilePath, null);
+	}
+
+	private String extractJar(String pid) {
 		String clazzFilePath = LoaderUtil.getClassFileName(AgentProviderMain.class.getName());
 		try {
 			InputStream is = this.getClass().getClassLoader().getResourceAsStream(clazzFilePath);
 			File jarFile = new File(FabricLoaderImpl.CACHE_DIR_NAME).toPath().resolve("fabric_agent_" + System.currentTimeMillis() + ".jar").toFile();
+			Files.createDirectories(jarFile.toPath().getParent());
 			if (jarFile.exists()) {
 				jarFile.delete();
 			}
@@ -94,20 +136,40 @@ public class ClassRedefineDelegateImpl<T extends ClassLoader & KnotClassDelegate
 			Manifest mf = new Manifest();
 			Attributes a = mf.getMainAttributes();
 			a.put(Attributes.Name.MANIFEST_VERSION, "1.0");
-			a.put("Agent-Class", "net.fabricmc.loader.impl.instrument.AgentProviderMain");
-			a.put("Can-Redefine-Classes", "true");
-			a.put("Can-Retransform-Classes", "true");
+			a.put(new Attributes.Name("Agent-Class"), "net.fabricmc.loader.impl.instrument.AgentProviderMain");
+			a.put(new Attributes.Name("Can-Redefine-Classes"), "true");
+			a.put(new Attributes.Name("Can-Retransform-Classes"), "true");
 			JarOutputStream jarOutputStream = new JarOutputStream(fileOS, mf);
 			ZipEntry classEntry = new ZipEntry(clazzFilePath);
 			jarOutputStream.putNextEntry(classEntry);
 			while (is.available() != 0) {
 				jarOutputStream.write(is.read());
 			}
-			jarOutputStream.close();
 			is.close();
+			jarOutputStream.putNextEntry(new ZipEntry("pid"));
+			jarOutputStream.write(pid.getBytes(StandardCharsets.UTF_8));
+			clazzFilePath = LoaderUtil.getClassFileName(AttachmentProvider.class.getName());
+			jarOutputStream.putNextEntry(new ZipEntry(clazzFilePath));
+			is = this.getClass().getClassLoader().getResourceAsStream(clazzFilePath);
+			while (is.available() != 0) {
+				jarOutputStream.write(is.read());
+			}
+			jarOutputStream.close();
+
 			return jarFile.getPath();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	private void clearAgentJars(){
+		File cacheDir = new File(FabricLoaderImpl.CACHE_DIR_NAME);
+		File[] files = cacheDir.listFiles((dir, name) -> name.contains("fabric_agent_") && dir.getPath().endsWith(".fabric"));
+		if (files == null)return;
+		for (File file : files) {
+			if (file.exists()){
+				file.delete();
+			}
 		}
 	}
 
